eca5c42372bf80a1860356dd2a87829e1cc0b2ba
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-05-24 22:30:23 -0500

    tweak h2 style

--- lib/mini_d20/node/h2.rb
+++ lib/mini_d20/node/h2.rb
@@ -1,10 +1,11 @@
 module MiniD20::Node
   module H2
     def self.process(node, pdf)
-      pdf.font "Book Antiqua", size: 12
+      pdf.font "Book Antiqua", size: 11
       pdf.text node.inner_html.strip, style: :bold
+      pdf.move_down 1
       pdf.stroke { pdf.horizontal_rule }
-      pdf.move_down 10
+      pdf.move_down 5
     end
   end
 end
