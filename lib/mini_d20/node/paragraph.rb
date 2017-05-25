5a487e2319a27719e18765d1784d3be6061076cf
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-05-24 23:02:34 -0500

    allow for line breaks in source paragraphs

--- lib/mini_d20/node/paragraph.rb
+++ lib/mini_d20/node/paragraph.rb
@@ -1,8 +1,10 @@
 module MiniD20::Node
   module Paragraph
     def self.process(node, pdf)
+      text = node.inner_html.gsub(/\n/, "").gsub(/\s+/, " ")
+
       pdf.font "Book Antiqua", size: 10
-      pdf.text node.inner_html.strip, inline_format: true
+      pdf.text text, inline_format: true
       pdf.move_down 10
     end
   end
