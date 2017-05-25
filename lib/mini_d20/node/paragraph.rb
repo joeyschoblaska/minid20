f9c107f122186b4033faf4c0b462c27939ad2fb9
Joey Schoblaska <joey.schoblaska@gmail.com>
2017-05-24 22:08:44 -0500

    strip paragraphs and add padding manually

--- lib/mini_d20/node/paragraph.rb
+++ lib/mini_d20/node/paragraph.rb
@@ -1,7 +1,8 @@
 module MiniD20::Node
   module Paragraph
     def self.process(node, pdf)
-      pdf.text node.inner_html, inline_format: true
+      pdf.text node.inner_html.strip, inline_format: true
+      pdf.move_down 10
     end
   end
 end
